d0236cd8b58d542d65962c06e2c5f92bad2e5ee4
Alan Casas <alancasasarevalo@gmail.com>
2021-08-07 11:11:44 +0200

    Initial Commit

---
+++ Piechart/PiechartApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiechartApp.swift
+//  Piechart
+//
+//  Created by Alan Casas on 7/8/21.
+//
+
+import SwiftUI
+
+@main
+struct PiechartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
